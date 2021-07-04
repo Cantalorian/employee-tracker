54e0fed325ec105a555ce932786979e455c03395
Chewie <chewie1985@gmail.com>
2021-07-04 02:35:02 -0600

    first query

--- index.js
+++ index.js
@@ -1,24 +1,87 @@
 const mysql = require('mysql2');
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 const table = require('console.table');
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    port: 3001,
-    // Your MySQL username,
     user: 'root',
-    // Your MySQL password
     password: 'Superstrongpassword3!',
     database: 'employees_db'
   },
-  console.log('Connected to the employee database.')
+  console.log(`Now connected to the employee database!`),
+  firstPrompt()
 );
 
-// connect to the mysql server and sql database
-db.connect(function (err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  firstPrompt();
-});
+function firstPrompt() {
+  inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+              "View all employees", 
+              "View all roles",
+              "View all departments",
+              "View employees by manager",
+              "Update an employee",
+              "Add an employee",
+              "Add a role",
+              "Add a department"
+            ]
+    }
+  ]).then(function(data) {
+    switch (data.choice) {
+      case "View all employees":
+            viewAllEmployees();
+            break;
+            
+      case "View all roles":
+            viewAllRoles();
+            break;
+
+      case "View all departments":
+            viewAllDepartments();
+            break;
+          
+      case "Add an employee":
+            addEmployee();
+            break;
+          
+      case "Update an employee":
+            updateEmployee();
+            break;
+              
+      case "Add a role":
+            addRole();
+            break;
+            
+      case "Add a department":
+            addDepartment();
+            break;
+            
+      case "View employees by manager":
+            viewByManager();
+            break;              
+    }
+  })
+};
+
+// View all employees in DB
+function viewAllEmployees() {
+  console.log(`===VIEWING EMPLOYEES===`);
+
+  let query = 
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;";
+  
+  db.query(query, function (err, employees) {
+    if (err) {
+      console.log(err);
+    }
+  
+    console.table(employees)
+  
+    firstPrompt()
+  });
+};
